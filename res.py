cf43ff3c65203819b006eaf30f498f219e83cd53
wesdresd11 <misterbizness777@tutamail.com>
2025-12-04 17:32:10 +0300

    Add files via upload

---
+++ res.py
@@ -0,0 +1,13 @@
+import notation
+import exchanges
+import Pascal
+import Euclid
+
+def main():
+    notation.main()
+    exchanges.main()
+    Pascal.main()
+    Euclid.main()
+
+if __name__ == '__main__':
+    main()
