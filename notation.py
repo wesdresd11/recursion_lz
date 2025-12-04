b8d0d981dae3829a1f83f73f0fd177179159a326
wesdresd11 <misterbizness777@tutamail.com>
2025-12-04 15:50:29 +0300

    Add files via upload

---
+++ notation.py
@@ -0,0 +1,11 @@
+
+def notation(number, base, upper=False):
+    digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+    if base > len(digits): return None
+    result = ''
+    while number > 0:
+        result = digits[number % base] + result
+        number //= base
+    return result.upper() if upper else result
+
+print(notation(12348, 4))
